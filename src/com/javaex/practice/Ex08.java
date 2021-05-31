e50cc881414464e35e20ee0ca1a50307e3f20e5e
jangjaeyeong <xkxh96@gmail.com>
2021-05-31 16:16:15 +0900

    5/31 coding(Arrar...)

--- src/com/javaex/practice/Ex08.java
+++ src/com/javaex/practice/Ex08.java
@@ -10,12 +10,18 @@ public class Ex08 {
 		for(int i = 0 ; i < minilotto.length; i++ ) {
 			
 			int lotto_num = (int)(Math.random() * 45)+ 1;
-			
 			minilotto[i] = lotto_num;
-		
-			System.out.print(minilotto[i] + "  ");
+			
+			for (int j = 0; j < i; j++) {
+				if(minilotto[i] == minilotto[j]) {
+					i--;
+					break;
+				}
+			}		
 		}
-		
+		for(int arraynum = 0; arraynum < minilotto.length; arraynum++) {
+			System.out.print(minilotto[arraynum] + "  ");
+		}
 	}
 
 }
